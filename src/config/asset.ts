61c16dee720357667ac72c0ad1af7f65324da9b8
亮程 <cl510804194@gmail.com>
2023-05-24 14:04:58 +0800

    fix:ipfs img bug

--- src/config/asset.ts
+++ src/config/asset.ts
@@ -1,4 +1,4 @@
 export const mockUser1 =
-  "ipfs://bafybeial6wdz3qshihfbq2ssdkb4yhqwvcvdw3eul4m2glrc6uivtucb5a";
+  'https://ipfs.io/ipfs/bafybeial6wdz3qshihfbq2ssdkb4yhqwvcvdw3eul4m2glrc6uivtucb5a';
 export const mockUser2 =
-  "ipfs://bafybeiah7yyfnpd3aiynqj7uigoum2julblgn2qfko6dvmitspd5mfx37a";
+  'https://ipfs.io/ipfs/bafybeiah7yyfnpd3aiynqj7uigoum2julblgn2qfko6dvmitspd5mfx37a';
